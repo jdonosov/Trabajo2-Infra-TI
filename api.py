9c9e5129bf91df202916443bd90158d3c3f753d1
Jose Donoso <j.donosovelsquez@uandresbello.edu>
2022-11-12 15:12:21 -0300

    api working

---
+++ api.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("indexEmpty.html")
+
+@app.route("/profile/genes/")
+def profile():
+    return render_template("indexEmpty.html")
+
+@app.route("/profile/genes/<geneName>/")
+def getGenes(geneName):
+    genes = ["RPS27L", "TSPAN6", "ENPP4", "C4orf27", "WSCD2"]
+
+    resp = requests.get("https://www.genenetwork.nl/api/v1/gene/" + geneName + "/",
+                        headers={'Accept': 'application/json'})
+
+    data = resp.json()
+
+    return render_template("index.html", geneData=data)
