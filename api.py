b82ba69ad825ecc4f3b5416eee19b25cfd9fc57d
Jose Donoso <j.donosovelsquez@uandresbello.edu>
2022-11-13 17:05:17 -0300

    using IP adress from VM

--- api.py
+++ api.py
@@ -12,11 +12,14 @@ def profile():
 
 @app.route("/profile/genes/<geneName>/")
 def getGenes(geneName):
-    genes = ["RPS27L", "TSPAN6", "ENPP4", "C4orf27", "WSCD2"]
-
+    #genes = ["RPS27L", "TSPAN6", "ENPP4", "C4orf27", "WSCD2"]
     resp = requests.get("https://www.genenetwork.nl/api/v1/gene/" + geneName + "/",
                         headers={'Accept': 'application/json'})
 
     data = resp.json()
 
     return render_template("index.html", geneData=data)
+
+if __name__ == "__main__":
+    Flask.run(app)
+    #Flask.run(app, host="0.0.0.0", port=80)
